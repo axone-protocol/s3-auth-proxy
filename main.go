10da34e3cf5909a58d016fadf13303502dd2e61a
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2024-03-13 12:16:51 +0100

    feat: make logs sexier

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "okp4/minio-auth-plugin/cmd"
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"okp4/minio-auth-plugin/cmd"
+	"os"
+)
 
 func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
 	cmd.Execute()
 }
