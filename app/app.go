5e365accaa8fdcf494ed55d4a4504b4df310152f
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2024-03-13 12:16:42 +0100

    feat: implement minio proxy

--- app/app.go
+++ app/app.go
@@ -1,23 +1,26 @@
 package app
 
 import (
+	"github.com/minio/minio-go/v7"
 	"github.com/rs/zerolog/log"
 	"okp4/minio-auth-plugin/auth"
 )
 
 type AuthApp struct {
 	listenAddr    string
+	minioClient   *minio.Client
 	authenticator *auth.Authenticator
 }
 
-func New(listenAddr string, authenticator *auth.Authenticator) *AuthApp {
+func New(listenAddr string, minioClient *minio.Client, authenticator *auth.Authenticator) *AuthApp {
 	return &AuthApp{
 		listenAddr:    listenAddr,
+		minioClient:   minioClient,
 		authenticator: authenticator,
 	}
 }
 
 func (a *AuthApp) Start() {
 	log.Info().Str("listenAddr", a.listenAddr).Msg("🔥 Listening")
-	log.Fatal().Err(configure(a.authenticator).ListenAndServe(a.listenAddr)).Msg("🛑 Shutting down")
+	log.Fatal().Err(configure(a.minioClient, a.authenticator).ListenAndServe(a.listenAddr)).Msg("🛑 Shutting down")
 }
--- cmd/start.go
+++ cmd/start.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/spf13/cobra"
 	"okp4/minio-auth-plugin/app"
 	"okp4/minio-auth-plugin/auth"
@@ -11,6 +13,9 @@ const (
 	FlagCognitariumAddr = "cognitarium-addr"
 	FlagServiceId       = "id"
 	FlagListenAddr      = "listen-addr"
+	FlagMinioURL        = "minio-url"
+	FlagMinioAccessKey  = "minio-access-key"
+	FlagMinioSecretKey  = "minio-secret-key"
 )
 
 var (
@@ -18,13 +23,28 @@ var (
 	cognitariumAddr string
 	serviceID       string
 	listenAddr      string
+	minioURL        string
+	minioAccessKey  string
+	minioSecretKey  string
 )
 
 var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "Launch the auth server",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		app.New(listenAddr, auth.New(nodeGrpcAddr, cognitariumAddr, serviceID)).Start()
+		minioClient, err := minio.New(minioURL, &minio.Options{
+			Creds:  credentials.NewStaticV4(minioAccessKey, minioSecretKey, ""),
+			Secure: false,
+		})
+		if err != nil {
+			return err
+		}
+
+		app.New(
+			listenAddr,
+			minioClient,
+			auth.New(nodeGrpcAddr, cognitariumAddr, serviceID),
+		).Start()
 
 		return nil
 	},
@@ -37,4 +57,7 @@ func init() {
 	startCmd.PersistentFlags().StringVar(&cognitariumAddr, FlagCognitariumAddr, "", "The cognitarium contract address")
 	startCmd.PersistentFlags().StringVar(&serviceID, FlagServiceId, "", "The service's identifier served")
 	startCmd.PersistentFlags().StringVar(&listenAddr, FlagListenAddr, "127.0.0.1:8080", "The server's listen address")
+	startCmd.PersistentFlags().StringVar(&minioURL, FlagMinioURL, "", "The proxied minio URL")
+	startCmd.PersistentFlags().StringVar(&minioAccessKey, FlagMinioAccessKey, "", "The minio access key")
+	startCmd.PersistentFlags().StringVar(&minioSecretKey, FlagMinioSecretKey, "", "The minio secret key")
 }
