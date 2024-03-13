b635fb3422152c11cf1b3c0a5e2f85bfad2ef3da
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2024-03-13 12:16:51 +0100

    feat: make logs sexier

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "okp4/s3-auth-proxy/cmd"
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"okp4/s3-auth-proxy/cmd"
+	"os"
+)
 
 func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
 	cmd.Execute()
 }
