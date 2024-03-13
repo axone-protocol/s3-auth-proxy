c630c23afffe814eb7705fbdc512304a5a499781
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2024-03-13 19:03:48 +0100

    feat: allow insecure grpc connections

--- cmd/start.go
+++ cmd/start.go
@@ -2,9 +2,11 @@ package cmd
 
 import (
 	"context"
+	"crypto/tls"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/spf13/cobra"
+	grpccreds "google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 	"okp4/s3-auth-proxy/app"
 	"okp4/s3-auth-proxy/auth"
@@ -13,27 +15,31 @@ import (
 )
 
 const (
-	FlagNodeGrpc      = "node-grpc"
-	FlagDataverseAddr = "dataverse-addr"
-	FlagServiceId     = "id"
-	FlagListenAddr    = "listen-addr"
-	FlagJWTSecretKey  = "jwt-secret-key"
-	FlagS3Endpoint    = "s3-endpoint"
-	FlagS3AccessKey   = "s3-access-key"
-	FlagS3SecretKey   = "s3-secret-key"
-	FlagS3Insecure    = "s3-insecure"
+	FlagNodeGrpc          = "node-grpc"
+	FlagGrpcNoTLS         = "grpc-no-tls"
+	FlagGrpcTLSSkipVerify = "grpc-tls-skip-verify"
+	FlagDataverseAddr     = "dataverse-addr"
+	FlagServiceId         = "id"
+	FlagListenAddr        = "listen-addr"
+	FlagJWTSecretKey      = "jwt-secret-key"
+	FlagS3Endpoint        = "s3-endpoint"
+	FlagS3AccessKey       = "s3-access-key"
+	FlagS3SecretKey       = "s3-secret-key"
+	FlagS3Insecure        = "s3-insecure"
 )
 
 var (
-	nodeGrpcAddr  string
-	dataverseAddr string
-	serviceID     string
-	listenAddr    string
-	jwtSecretKey  []byte
-	s3Endpoint    string
-	s3AccessKey   string
-	s3SecretKey   string
-	s3Insecure    bool
+	nodeGrpcAddr      string
+	grpcNoTls         bool
+	grpcTlsSkipVerify bool
+	dataverseAddr     string
+	serviceID         string
+	listenAddr        string
+	jwtSecretKey      []byte
+	s3Endpoint        string
+	s3AccessKey       string
+	s3SecretKey       string
+	s3Insecure        bool
 )
 
 var startCmd = &cobra.Command{
@@ -50,7 +56,7 @@ var startCmd = &cobra.Command{
 
 		ctx, cancelFn := context.WithTimeout(context.Background(), 2*time.Second)
 		defer cancelFn()
-		dataverseClient, err := dataverse.NewClient(ctx, nodeGrpcAddr, dataverseAddr, insecure.NewCredentials())
+		dataverseClient, err := dataverse.NewClient(ctx, nodeGrpcAddr, dataverseAddr, getTransportCredentials())
 		if err != nil {
 			return err
 		}
@@ -69,6 +75,11 @@ func init() {
 	rootCmd.AddCommand(startCmd)
 
 	startCmd.PersistentFlags().StringVar(&nodeGrpcAddr, FlagNodeGrpc, "127.0.0.1:9090", "The node grpc address")
+	startCmd.PersistentFlags().BoolVar(&grpcNoTls, FlagGrpcNoTLS, false, "No encryption with the GRPC endpoint")
+	startCmd.PersistentFlags().BoolVar(&grpcTlsSkipVerify,
+		FlagGrpcTLSSkipVerify,
+		false,
+		"Encryption with the GRPC endpoint but skip certificates verification")
 	startCmd.PersistentFlags().StringVar(&dataverseAddr, FlagDataverseAddr, "", "The dataverse contract address")
 	startCmd.PersistentFlags().StringVar(&serviceID, FlagServiceId, "", "The service's identifier served")
 	startCmd.PersistentFlags().StringVar(&listenAddr, FlagListenAddr, "127.0.0.1:8080", "The server's listen address")
@@ -77,4 +88,15 @@ func init() {
 	startCmd.PersistentFlags().StringVar(&s3AccessKey, FlagS3AccessKey, "", "The S3 access key")
 	startCmd.PersistentFlags().StringVar(&s3SecretKey, FlagS3SecretKey, "", "The S3 secret key")
 	startCmd.PersistentFlags().BoolVar(&s3Insecure, FlagS3Insecure, false, "If specified we'll accept non encrypted connection with the S3")
+}
+
+func getTransportCredentials() grpccreds.TransportCredentials {
+	switch {
+	case grpcNoTls:
+		return insecure.NewCredentials()
+	case grpcTlsSkipVerify:
+		return grpccreds.NewTLS(&tls.Config{InsecureSkipVerify: true}) // #nosec G402 : skip lint since it's an optional flag
+	default:
+		return grpccreds.NewTLS(&tls.Config{MinVersion: tls.VersionTLS12})
+	}
 }
