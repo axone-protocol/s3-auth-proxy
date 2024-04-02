7025931e7a20a2c6838c998d511e546488096419
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2024-04-02 11:04:42 +0200

    chore: update example dids

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,7 +10,7 @@ fi
 BASEDIR=$(dirname $0)
 
 ./${BASEDIR}/submit-vc.sh "${BASEDIR}/../example/vc-s3-desc.jsonld" s3-issuer $1 $2
-./${BASEDIR}/submit-vc.sh "${BASEDIR}/../example/vc-s3-gov.jsonld" s3-issuer $1 $2
+./${BASEDIR}/submit-vc.sh "${BASEDIR}/../example/vc-s3-gov.jsonld" s3 $1 $2
 ./${BASEDIR}/submit-vc.sh "${BASEDIR}/../example/vc-data-desc.jsonld" data-issuer $1 $2
-./${BASEDIR}/submit-vc.sh "${BASEDIR}/../example/vc-data-gov.jsonld" data-issuer $1 $2
+./${BASEDIR}/submit-vc.sh "${BASEDIR}/../example/vc-data-gov.jsonld" data $1 $2
 ./${BASEDIR}/submit-vc.sh "${BASEDIR}/../example/vc-publish.jsonld" s3-issuer $1 $2
